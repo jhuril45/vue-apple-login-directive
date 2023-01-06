63cf13884fa652f8c0c8947073e16f3116ed9f2c
jhuril45 <jhuril45@gmail.com>
2023-01-06 10:25:48 +0800

    Updates on script

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import directive from "./directive";
-import appleApi from "./appleApi.js";
-// const api_script = 'https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1/en_US/appleid.auth.js'
+// import appleApi from "./appleApi.js";
+const api_script = 'https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1/en_US/appleid.auth.js'
 
-// let appleSignInAPI = document.createElement("script");
-// appleSignInAPI.setAttribute(
-//   "src",
-//   api_script
-// );
-// document.head.appendChild(appleSignInAPI);
-eval(appleApi)
+let appleSignInAPI = document.createElement("script");
+appleSignInAPI.setAttribute(
+  "src",
+  api_script
+);
+document.head.appendChild(appleSignInAPI);
+// eval(appleApi)
 
 const plugin = {
   install(Vue) {
